59bacd860f84fd57d8a36826f8c5611fb18e10ab
Milan Grubnić <MilanG@aol.com>
2016-02-04 20:04:15 -0600

    got both players moving base on their individual keyup events

--- _js/script.js
+++ _js/script.js
@@ -2,17 +2,27 @@ function init() {
     var FirstPlayerPosition = 1;
     var SecondPlayerPosition = 1;
 
-    function update_player(n) {
+    function update_player_1(n) {
         FirstPlayerPosition += n;
         $('td.active').toggleClass();
         $("tr#1stPlayerLane td:nth-child(" + FirstPlayerPosition + ")").addClass("active");
+    }
+
+    function update_player_2(n) {
+        SecondPlayerPosition += n;
+        $('tr#2ndPlayerLane td.active').toggleClass();
+        $("tr#2ndPlayerLane td:nth-child(" + SecondPlayerPosition + ")").addClass("active");
     }
 
     $(document).on('keyup', function(event) {
         console.log(event.which);
 
         if (event.which == 81) {
-            update_player(1);
+            update_player_1(1);
+        }
+
+        if (event.which == 80) {
+            update_player_2(1);
         }
 
 
