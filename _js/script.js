c929a5bf28c6393b84ac3123e69bb6d1f0118f2f
Milan Grubnić <MilanG@aol.com>
2016-02-04 19:53:41 -0600

    got player to move to next spot with keyup

--- _js/script.js
+++ _js/script.js
@@ -1,10 +1,55 @@
 function init() {
-    alert("Start your engines!");
-    $('.active').click(function() {
-        $(this).toggleClass();
-        $(this).next().toggleClass('active');
-        
+    var FirstPlayerPosition = 0;
+    var SecondPlayerPosition = 0;
+
+
+
+
+    function update_player(n) {
+        FirstPlayerPosition += n;
+        x = $('td.active');
+        // console.log(next);
+        x.toggleClass();
+        // $('td.active').next().toggleClass('.active');
+        // x: child('.active').toggleClass();
+
+        $("td.active").next().addClass("active");
+        // $( "td" ).sibling().addClass( "active" );
+
+        $("tr td:nth-child(" + FirstPlayerPosition + ")").addClass("active");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    $(document).on('keyup', function(event) {
+        console.log(event.which);
+
+        if (event.which == 81) {
+            update_player(1);
+        }
+
+
     })
+
+
+
+
+
+
+
+
+
+
+
 }
 
 $(document).ready(function() {
