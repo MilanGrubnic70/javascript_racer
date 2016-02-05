d64f51e2bc6ad0831ccfaf1ba5fc45cbcc4a4a61
Milan Grubnić <MilanG@aol.com>
2016-02-04 20:21:11 -0600

    refactored & added comments

--- _js/script.js
+++ _js/script.js
@@ -7,37 +7,37 @@ function init() {
         if (spot == 10) {
             alert("Player " + player + " wins!!!");
             location.reload();
-        }
-    }
+        } // if
+    } // checkered_flag
 
     function update_player_1(arg) {
         FirstPlayerPosition += arg;
         $('tr#1stPlayerLane td.active').toggleClass();
         $("tr#1stPlayerLane td:nth-child(" + FirstPlayerPosition + ")").addClass("active");
         checkered_flag("One", FirstPlayerPosition);
-    }
+    } // update_player_1
 
     function update_player_2(arg) {
         SecondPlayerPosition += arg;
         $('tr#2ndPlayerLane td.active').toggleClass();
         $("tr#2ndPlayerLane td:nth-child(" + SecondPlayerPosition + ")").addClass("active");
-    }
+        checkered_flag("Two", SecondPlayerPosition);
+    } // update_player_2
 
     $(document).on('keyup', function(event) {
-        console.log(event.which);
+            console.log(event.which);
 
-        if (event.which == 81) {
-            update_player_1(1);
-        }
+            if (event.which == 81) {
+                update_player_1(1);
+            } // if
 
-        if (event.which == 80) {
-            update_player_2(1);
-        }
+            if (event.which == 80) {
+                update_player_2(1);
+            } // if
 
+        }) // on
 
-    })
-
-}
+} // init
 
 $(document).ready(function() {
     init();
