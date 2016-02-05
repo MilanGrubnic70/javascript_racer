35a4fcf77f31081653e10164dcf9b402d7a735c4
Milan Grubnić <MilanG@aol.com>
2016-02-04 19:59:15 -0600

    refactored

--- _js/script.js
+++ _js/script.js
@@ -2,25 +2,11 @@ function init() {
     var FirstPlayerPosition = 1;
     var SecondPlayerPosition = 1;
 
-
-
-
     function update_player(n) {
         FirstPlayerPosition += n;
-        x = $('td.active');
-        x.toggleClass();
+        $('td.active').toggleClass();
         $("tr#1stPlayerLane td:nth-child(" + FirstPlayerPosition + ")").addClass("active");
     }
-
-
-
-
-
-
-
-
-
-
 
     $(document).on('keyup', function(event) {
         console.log(event.which);
